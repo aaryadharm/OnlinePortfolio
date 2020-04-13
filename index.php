3fdbb8eee9044f61ff441a9d2308a81a2e62a86b
aaryadharm <57190184+aaryadharm@users.noreply.github.com>
2020-04-13 15:12:03 -0700

    Added all files on repl.it portfolio

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head class="container">
+    <title>PHP Test</title>
+    <nav>
+      <a href="index.html">Home</a>
+      <a href="Dharm_aboutme.html">About Me</a>
+      <a href="Dharm_projects.html">Projects</a>
+      <a href="Dharm_WorkExperience.html">Resume</a>
+      <a href="Dharm_contactme.html">Contact Me</a>
+    </nav>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Aarya Dharm; Period 1]</p>'; ?> 
+    
+  </body>
+</html>
